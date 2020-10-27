6e5c57e66e12c4b9bb4a087153c16a5e83217410
Luke <luke.dobbyn@pcc.edu>
2020-10-26 21:23:46 -0700

    Added print to print out current date

--- src/edu/pcc/cis/helloWorld/HelloWorld.java
+++ src/edu/pcc/cis/helloWorld/HelloWorld.java
@@ -1,8 +1,11 @@
 package edu.pcc.cis.helloWorld;
 
+import java.util.Date;
+
 public class HelloWorld {
     public static void main(String args[])
     {
         System.out.println("Hello, world!");
+        System.out.println(new Date());
     }
 }
