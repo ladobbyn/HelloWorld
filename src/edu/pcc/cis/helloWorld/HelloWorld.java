9cb9f6004af0a8ea98c30f7de0e49266d26af249
Luke <luke.dobbyn@pcc.edu>
2020-10-26 21:25:08 -0700

    Forgot to add author and version, added them here

--- src/edu/pcc/cis/helloWorld/HelloWorld.java
+++ src/edu/pcc/cis/helloWorld/HelloWorld.java
@@ -2,6 +2,11 @@ package edu.pcc.cis.helloWorld;
 
 import java.util.Date;
 
+/**
+ * A class just to test out IntelliJ and github
+ * @author Luke Dobbyn
+ * @version 2020.10.26
+ */
 public class HelloWorld {
     public static void main(String args[])
     {
